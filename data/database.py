71a26502efe18cbc02a90502776986da0b57fad2
Jonal Ray  G. Ticug <11526785@dlsu.edu.ph>
2019-02-06 19:13:13 +0800

    Added the create table query

---
+++ data/database.py
@@ -0,0 +1,67 @@
+import sqlite3
+
+tables = {
+    'vocabulary': {
+        'columns': {
+            'id': { "type": 'INTEGER', "primary_key": True },
+            'name': { "type": 'TEXT' },
+            'count': { "type": 'INTEGER' }
+        }
+    },
+    'matrix': {
+        'columns': {
+            'id': { "type": 'INTEGER', "primary_key": True },
+            'mistake': { "type": 'TEXT' },
+            'correct': { "type": 'TEXT'},
+            'count': { "type": 'INTEGER' }
+        }
+    }
+}
+
+def create_table_string (table_data, table_name):
+    message = "CREATE TABLE {} ".format(table_name)
+
+    column_count = 0
+    message += "("
+    for column_name in table_data['columns']:
+        column_data = table_data['columns'][column_name]
+        message += "{} {}{}".format(column_name, column_data['type'], (" PRIMARY KEY" if hasattr(column_data, "primary_key") else ""))
+        column_count += 1
+        if (column_count != len(table_data['columns'])):
+            message += ", "
+
+    message += ")"
+
+    return message
+
+def create_database ():
+    # connection = sqlite3.connect("data.sqlite")
+    # cursor = conenction.cursor()
+    
+    for table_name in tables:
+        table_data = tables[table_name]
+        print(create_table_string(table_data, table_name))
+
+create_database() 
+
+# sqlite_file = 'gram.sqlite'    # name of the sqlite database file
+# table_name1 = 'my_table_1'  # name of the table to be created
+# table_name2 = 'my_table_2'  # name of the table to be created
+# new_field = 'my_1st_column' # name of the column
+# field_type = 'INTEGER'  # column data type
+
+# # Connecting to the database file
+# c = conn.cursor()
+
+# # Creating a new SQLite table with 1 column
+# c.execute('CREATE TABLE {tn} ({nf} {ft})'\
+#         .format(tn=table_name1, nf=new_field, ft=field_type))
+
+# # Creating a second table with 1 column and set it as PRIMARY KEY
+# # note that PRIMARY KEY column must consist of unique values!
+# c.execute('CREATE TABLE {tn} ({nf} {ft} PRIMARY KEY)'\
+#         .format(tn=table_name2, nf=new_field, ft=field_type))
+
+# # Committing changes and closing the connection to the database file
+# conn.commit()
+# conn.close()
---
+++ spelling_error.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from collections import Counter
+import re
+import os
+
+def log (message):
+    print("[{}] {}".format(time.strftime("%Y-%m-%d %H:%M:%S", time.time()), message))
+
+def load_corpus (directory):
+    text_files = []
+    term_counter = Counter()
+
+    log("Loading corpus")
+    for folder in directory:
+        file_names = os.listdir(folder)
+
+        for file_name in file_names[:]:
+            with open(folder + "/" + file_name, 'r', encoding='utf8') as f:
+                title = file_name[:-4]
+                temp = {
+                    "Title": title,
+                    "Raw Text": f.read()
+                }
+                temp['Tokens'] = re.findall(re_pattern, temp["Raw Text"].lower())
+                temp['Vocabulary'] = list(set(re.findall(re_pattern, temp["Raw Text"].lower())))
+                
+                print("===%s===" % title)
+                print("Total tokens: %s" % len(temp['Tokens']))
+                print("Total vocabulary: %s" % len(temp['Vocabulary']))
+
+                text_files.append(temp)
+                term_counter.update(temp['Tokens'])
+    log("Loading corpus: Done!")
+    return text_files, term_counter
+
+def identify_vocabulary(text_files):
+    total_vocabulary = set()
+    for song in text_files:
+        total_vocabulary |= set(song['Vocabulary'])
+    total_vocabulary = list(total_vocabulary)
+    print("Vocabulary count: {}".format(len(total_vocabulary)))
+    return total_vocabulary
+
+
+
+
